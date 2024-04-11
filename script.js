43f8e827376a54addc2c5667ee87bebdfb30a0fc
PROJECT1205 <165776596+PROJECT1205@users.noreply.github.com>
2024-04-11 13:12:07 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Toggle mobile menu
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    var menuItems = document.querySelector('.menu-items');
+    menuItems.classList.toggle('open');
+});
